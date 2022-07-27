ae19cd4fe4fa58b8b8749b4b2a4755e073388a85
Tweeel <houssembababendermel@gmail.com>
2022-07-27 15:53:13 +0100

    week page ui

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.weatherapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        week.setOnClickListener {
+            startActivity(Intent(this,WeekActivity::class.java))
+        }
 
     }
 }
--- app/src/main/java/com/example/weatherapp/ui/WeekActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeekActivity.kt
@@ -3,10 +3,16 @@ package com.example.weatherapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapp.R
+import kotlinx.android.synthetic.main.activity_week.*
 
 class WeekActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_week)
+        supportActionBar?.hide()
+
+        rollback.setOnClickListener{
+            finish()
+        }
     }
 }
